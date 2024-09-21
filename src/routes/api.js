2fefef57408e4db3aa15acde5a89335e8bf15992
VuManh01 <vutrongmanhmk@gmail.com>
2024-09-21 11:06:04 +0700

    step10

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,8 +4,11 @@ const {
   handleLogin,
   getUser,
 } = require("../controllers/userController");
+const delay = require("../middleware/delay");
 
 const routerAPI = express.Router();
+
+routerAPI.all("*", delay);
 
 routerAPI.get("/", (req, res) => {
   return res.status(200).json("Hello world API");
